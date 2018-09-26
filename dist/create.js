b133e064178ff22d2e1849c20e7ab53b0b40fcd7
Leonardo Delfino <leonardodelfino@gmail.com>
2018-09-26 17:01:18 -0300

    Novas funcionalidades

--- dist/create.js
+++ dist/create.js
@@ -1,8 +1,11 @@
 var ByteArrayOutputStream = Java.type('java.io.ByteArrayOutputStream');
 var CellType = Java.type('org.apache.poi.ss.usermodel.CellType');
 var HorizontalAlignment = Java.type('org.apache.poi.ss.usermodel.HorizontalAlignment');
+var VerticalAlignment = Java.type('org.apache.poi.ss.usermodel.VerticalAlignment');
 var XSSFFont = Java.type('org.apache.poi.xssf.usermodel.XSSFFont');
 var XSSFWorkbook = Java.type('org.apache.poi.xssf.usermodel.XSSFWorkbook');
+var IndexedColors = Java.type('org.apache.poi.ss.usermodel.IndexedColors');
+var FillPatternType = Java.type('org.apache.poi.ss.usermodel.FillPatternType');
 
 var DEFAULT_DATE_FORMAT = 'DD/MM/yyyy';
 var DEFAULT_TIME_FORMAT = 'HH:MM';
@@ -10,7 +13,7 @@ var DEFAULT_DATETIME_FORMAT = DEFAULT_DATE_FORMAT + ' ' + DEFAULT_TIME_FORMAT;
 
 var DEFAULT_CURRENCY_FORMAT = 'R$ #,##0.00';
 
-var FONT_OPTION_NAMES = ['fontName', 'fontSize', 'bold', 'italic', 'striked', 'underline', 'doubleUnderline'];
+var FONT_OPTION_NAMES = ['fontName', 'fontSize', 'fontColor', 'bold', 'italic', 'striked', 'underline', 'doubleUnderline'];
 var FONTS_CACHE = {};
 var STYLES_CACHE = {};
 
@@ -29,21 +32,44 @@ function create(rows, metadata) {
     var createHelper = wb.getCreationHelper();
     var sheet = wb.createSheet();
 
+    var sheetProperties = metadata.sheetProperties;
     var columnsMD = metadata.columns;
     var generalStyle = metadata.style;
     var headerStyleMD = metadata.headerStyle;
+    var rowStyle = metadata.rowStyle;
 
-    rows.forEach(function (row, index) {
+    if (sheetProperties) {
+      if (sheetProperties.password) {
+        sheet.protectSheet(sheetProperties.password);
+        sheet.lockDeleteColumns(true);
+        sheet.lockInsertColumns(true);
+      }
+
+      if (sheetProperties.name) {
+        wb.setSheetName(0, sheetProperties.name);
+      }
+    }
+
+    rows.forEach(function(row, index) {
       var sheetHeader = metadata.hasHeader && index == 0 ? sheet.createRow(0) : null;
       var sheetRow = sheet.createRow(metadata.hasHeader ? index + 1 : index);
 
-      Object.keys(row).forEach(function (key, cellIndex) {
+      Object.keys(row).forEach(function(key, cellIndex) {
         var value = row[key];
         var valueClass = value && value.constructor.name;
 
         var columnMD = columnsMD && columnsMD[key];
 
+        if (rowStyle && rowStyle.height) {
+          sheetRow.setHeight(rowStyle.height);
+        }
+
         if (sheetHeader != null) {
+
+          if (headerStyleMD.height) {
+            sheetHeader.setHeight(rowStyle.height);
+          }
+
           var style = Object.assign({}, generalStyle, columnMD, headerStyleMD);
 
           var headerCell = createCell(wb, sheetHeader, cellIndex, style, createHelper, valueClass);
@@ -57,14 +83,31 @@ function create(rows, metadata) {
 
         var style = Object.assign({}, generalStyle, columnMD);
         var cell = createCell(wb, sheetRow, cellIndex, style, createHelper, valueClass);
-        setTypedValue(cell, value, valueClass);
+
+        var formula = (columnMD && columnMD.formula) ? columnMD.formula : null;
+        setTypedValue(cell, value, valueClass, formula);
       });
     });
 
     if (metadata.autoSize) {
       var headerRow = sheet.getRow(0);
-      headerRow.cellIterator().forEachRemaining(function (cell) {
+      headerRow.cellIterator().forEachRemaining(function(cell) {
         sheet.autoSizeColumn(cell.getColumnIndex());
+      });
+    } else {
+      var headerRow = sheet.getRow(0);
+      headerRow.cellIterator().forEachRemaining(function(cell) {
+        var columnsKeys = Object.keys(columnsMD);
+        var columnIndex = cell.getColumnIndex()
+
+        if (columnsMD && columnsKeys.length > 0) {
+          var columnMD = columnsMD[columnsKeys[columnIndex]]
+          if (columnMD.size && columnMD.size.toString().toUpperCase() === 'AUTO') {
+            sheet.autoSizeColumn(cell.getColumnIndex());
+          } else if (!isNaN(columnMD.size) && columnMD.size >= 0) {
+            sheet.setColumnWidth(cell.getColumnIndex(), columnMD.size);
+          }
+        }
       });
     }
 
@@ -90,12 +133,12 @@ function createCell(wb, sheetRow, cellIndex, style, createHelper, valueClass) {
   if (Object.keys(style).length) {
     var cacheKey = JSON.stringify(style);
     var cellStyle = STYLES_CACHE[cacheKey];
-  
+
     if (!cellStyle) {
       cellStyle = wb.createCellStyle();
-      
+
       updateCellStyle(wb, cellStyle, style, createHelper);
-      
+
       STYLES_CACHE[cacheKey] = cellStyle;
     }
 
@@ -110,8 +153,17 @@ function updateCellStyle(wb, cellStyle, options, createHelper) {
     return;
   }
 
+  if (options.backgroundColor) {
+    cellStyle.setFillForegroundColor(IndexedColors[options.backgroundColor.toUpperCase()].getIndex());
+    cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+  }
+
   if (options.format) {
     cellStyle.setDataFormat(createHelper.createDataFormat().getFormat(options.format));
+  }
+
+  if(options.unlocked) {
+    cellStyle.setLocked(false);
   }
 
   var font = resolveFont(wb, options);
@@ -123,6 +175,9 @@ function updateCellStyle(wb, cellStyle, options, createHelper) {
   if (options.horizontalAlignment) {
     cellStyle.setAlignment(HorizontalAlignment[options.horizontalAlignment.toUpperCase()]);
   }
+  if (options.verticalAlignment) {
+    cellStyle.setVerticalAlignment(VerticalAlignment[options.verticalAlignment.toUpperCase()]);
+  }
 }
 
 function resolveFont(wb, options) {
@@ -130,7 +185,7 @@ function resolveFont(wb, options) {
     return undefined;
   }
 
-  var fontOpts = FONT_OPTION_NAMES.reduce(function (map, optName) {
+  var fontOpts = FONT_OPTION_NAMES.reduce(function(map, optName) {
     if (options[optName]) {
       map[optName] = options[optName];
     }
@@ -171,6 +226,10 @@ function resolveFont(wb, options) {
         font.setUnderline(0x22);
       }
 
+      if (fontOpts.fontColor) {
+        font.setColor(IndexedColors[fontOpts.fontColor.toUpperCase()].getIndex());
+      }
+
       FONTS_CACHE[key] = font;
     }
   }
@@ -198,7 +257,8 @@ function resolveDataFormat(options, valueClass) {
   }
 }
 
-function setTypedValue(cell, value, valueClass) {
+function setTypedValue(cell, value, valueClass, formula) {
+
   if (value === null || value === undefined) {
     cell.setCellType(CellType.BLANK);
     return;
@@ -208,7 +268,13 @@ function setTypedValue(cell, value, valueClass) {
     value = new java.util.Date(value.getTime());
   }
 
-  cell.setCellValue(value);
+  if (formula) {
+    cell.setCellType(CellType.FORMULA);
+    cell.setCellFormula(value);
+  } else {
+    cell.setCellValue(value);
+  }
+
 }
 
 exports = create;
